3045f6d2e215313a2cfe2d8ed68da3798a36e69d
Rafaela Cristina <betelgeusin@gmail.com>
2022-03-17 00:52:07 -0300

    Adicionando última parte do módulo 4 e finalizando o projeto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,37 @@
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao";
+import Link from "next/link"
+import EntradaNumerica from "../components/EntradaNumerica";
 import { useState } from "react";
-import Porta from "../components/Porta";
-import {atualizarPortas, criarPortas} from "../functions/porta"
-import PortaModel from "../model/porta";
 
-export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
+export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
 
-  function renderizarPortas(){
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} 
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}/>
-    })
-  }
-
-  return (
-    <div style={{display: "flex"}}>
-      {renderizarPortas()}
+  return(
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao>
+          <EntradaNumerica text="Qtde Portas?" 
+            value={qtdePortas}
+            onChange={novaQtde => setQtdePortas(novaQtde)}/>
+        </Cartao>
+      </div>
+      <div>
+        <Cartao>
+          <EntradaNumerica text="Porta com Presente?" 
+              value={comPresente}
+              onChange={novaPortaComPresente => setComPresente(novaPortaComPresente)}/>
+        </Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/${qtdePortas}/${comPresente}`} passHref>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }
